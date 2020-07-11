67634bbcced0f0890128d50e045b253a2084525e
Kubus Puchatek <msoftlive@hotmail.com>
2020-07-11 13:47:00 +0200

    Event and participant models added

---
+++ LapZaWioslo/Models/Participant.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LapZaWioslo.Models
+{
+    [Table("Participant")]
+    public class Participant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("EventInformation")]
+        public int EventId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+
+        public virtual EventInformation EventInformation { get; set; }
+    }
+}
